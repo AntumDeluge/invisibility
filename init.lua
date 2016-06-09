d8502addc57fb79a381b7d9bb96856da0ed96a97
TenPlus1 <kinsellaja@yahoo.com>
2016-06-09 13:52:40 +0100

    Fixed bug where recipe didn't show in crafting guide

--- init.lua
+++ init.lua
@@ -28,10 +28,7 @@ minetest.register_node("invisibility:potion", {
 		type = "fixed",
 		fixed = {-0.25, -0.5, -0.25, 0.25, 0.3, 0.25}
 	},
-	groups = {
-		vessel = 1, dig_immediate = 3, attached_node = 1,
-		not_in_creative_inventory = 1
-	},
+	groups = {vessel = 1, dig_immediate = 3, attached_node = 1},
 	sounds = default.node_sound_glass_defaults(),
 
 	on_use = function(itemstack, user)
